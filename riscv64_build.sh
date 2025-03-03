f30580926f8d8e2252a9ff120f55ed9db08a7a4b
zhangyang2057 <zhangyang@canaan-creative.com>
2025-03-03 17:31:17 +0800

    Add 3rd_party(nncase/mmz/gsl/onnxruntime).

---
+++ riscv64_build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -x
+
+# set cross build toolchain and k230 sdk(Linux)
+export PATH=$PATH:`pwd`/toolchain/Xuantie-900-gcc-linux-6.6.0-glibc-x86_64-V2.10.1/bin
+build_type=Release
+# build_type=Debug
+
+clear
+build=riscv64_build
+rm -rf ${build}
+mkdir ${build}
+pushd ${build}
+cmake -DCMAKE_BUILD_TYPE=${build_type}           \
+      -DCMAKE_INSTALL_PREFIX=`pwd`               \
+      -DCMAKE_TOOLCHAIN_FILE=cmake/Riscv64.cmake \
+      -DDUMP_PROFILE_INFO=1 \
+      ..
+
+make -j
+popd
+
+cp ${build}/cli_demo /home/share/nfsroot/k230/k230_llm/onnx-llm
