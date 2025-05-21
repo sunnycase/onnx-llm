d2f0935b7a3f525096ea23a6965ec731bba08c32
zhangyang2057 <zhangyang@canaan-creative.com>
2025-05-21 15:16:59 +0800

    Update nncase both riscv and native runtime for qwen2.5

--- 3rd_party/nncase/riscv64/get.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -rf bin lib include
-
-cp -r /home/zhangyang/workspace/k230_conan2_test/nncase_linux_runtime/* .
--- 3rd_party/nncase/x86_64/get.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -rf bin lib include python
-
-cp -r /home/zhangyang/workspace/k230_conan2_test/nncase_native_runtime/* .
